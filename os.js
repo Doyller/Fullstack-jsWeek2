d758b3ef43bfea076e9558b3f1eeda80cca9fbd0
Doyller <117834332+Doyller@users.noreply.github.com>
2023-05-09 14:37:06 -0230

    changes

--- os.js
+++
@@ -1,7 +0,0 @@
-var os = require('os');
-
-console.log("Platform: " + os.platform());
-console.log("Architecture: " + os.arch());
-
-console.log("hostname: " + os.hostname());
-console.log("freemem: " + os.freemem());
